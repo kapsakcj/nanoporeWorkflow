e7eb401d7f764dc0b8ce63179ac78d7da902b0d8
Lee Katz - Aspen <gzu2@cdc.gov>
2019-07-15 14:22:47 -0400

    workflow updated for medaka. Only one uuid generated per barcode.

--- workflows/workflow.sh
+++ workflows/workflow.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-#$ -o wtdbg2.log
+#$ -o np_workflow.log
 #$ -j y
-#$ -N wtdbg2
+#$ -N np_workflow
 #$ -pe smp 1-36
 #$ -V -cwd
 #$ -l gpu=1
 set -e
-
-source /etc/profile.d/modules.sh
-module purge
 
 NSLOTS=${NSLOTS:=24}
 
@@ -41,27 +38,25 @@ echo "$0: temp dir is $tmpdir";
 uuid1=$(uuidgen)
 jobName1="basecall-$uuid1"
 qsub -pe smp 1-$NSLOTS -N $jobName1 -cwd -o log/$jobName1.log -j y \
-  01_basecall.sh $OUTDIR $FAST5DIR
+  np_basecall-demux_guppy.sh $OUTDIR $FAST5DIR
 
 # Now that it is demultiplexed, deal with each sample at a time.
-for barcodeDir in $OUTDIR/demux/barcode[0-9]*; do
+for barcodeDir in $OUTDIR/barcode[0-9]*; do
+  barcodeuuid=$(uuidgen)
 
   # Prep the sample
-  uuid2=$(uuidgen)
-  jobName2="prepSample-$uuid2"
+  jobName2="prepSample-$barcodeuuid"
   qsub -hold_jid $jobName1 -pe smp 1-$NSLOTS -N $jobName2 -cwd -o log/$jobName2.log -j y \
-    03_prepSample.sh $barcodeDir
+    np_prepSample_readLengths.sh $barcodeDir
   
   # Assemble the sample
-  uuid3=$(uuidgen)
-  jobName3="assemble-$uuid3"
+  jobName3="assemble-$barcodeuuid"
   qsub -hold_jid $jobName2 -pe smp 1-$NSLOTS -N $jobName3 -cwd -o log/$jobName3.log -j y \
-    05_assemble.sh $barcodeDir
+    np_assemble_wtdbg2.sh $barcodeDir
 
   # Polish the sample
-  uuid4=$(uuidgen)
-  jobName4="assemble-$uuid4"
+  jobName4="polish-$barcodeuuid"
   qsub -hold_jid $jobName3 -pe smp 1-$NSLOTS -N $jobName4 -cwd -o log/$jobName4.log -j y \
-    07_nanopolish.sh $barcodeDir $FAST5DIR
+    np_polish_medaka.sh $barcodeDir $FAST5DIR
 
 done
