98c401a17846c4a7c40d19f3646c838122176356
Lee Katz - Aspen <gzu2@cdc.gov>
2019-07-17 15:12:01 -0400

    chiron

---
+++ scripts/np_basecall-demux_chiron.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#$ -o wtdbg2.log
+#$ -e wtdbg2.err
+#$ -j y
+#$ -N wtdbg2
+#$ -pe smp 2-16
+#$ -V
+#$ -cwd
+set -e
+
+source /etc/profile.d/modules.sh
+
+NSLOTS=${NSLOTS:=24}
+
+OUTDIR=$1
+FAST5DIR=$2
+GENOMELENGTH=5000000 # TODO make this a parameter
+LONGREADCOVERAGE=50  # How much coverage to target with long reads
+
+set -u
+
+if [ "$FAST5DIR" == "" ]; then
+    echo "Usage: $0 outdir fast5dir/"
+    echo "  The outdir will represent each sample in a 'barcode__' subdirectory"
+    exit 1;
+fi;
+
+# Setup any debugging information
+date
+hostname
+
+# Setup tempdir
+tmpdir=$(mktemp -p . -d chiron-qcat.XXXXXX)
+trap ' { echo "END - $(date)"; rm -rf $tmpdir; } ' EXIT
+mkdir $tmpdir/log
+echo "$0: temp dir is $tmpdir";
+
+module purge
+module load gcc/5.5
+
+# Test whether chiron is in the path
+#chiron --help >/dev/null 
+
+echo "Separating multifast5 files to single for compatibility with Chiron"
+multi_to_single_fast5 -i $FAST5DIR --threads $NSLOTS -s $tmpdir/FAST5.single
+
+# Base calling
+modeldir=$(find $HOME/.local/lib/*/site-packages -mindepth 2 -type d -name DNA_default | head -n 1)
+echo "Model can be found at $modeldir"
+chiron call -i $tmpdir/FAST5.single -o $tmpdir/chiron -m $modeldir -t $NSLOTS
+
+# Demultiplex with qcat
+find $tmpdir/chiron -type f -name '*.fast5' -exec cat {} | \
+  qcat -b $tmpdir/demux
+
+mv $tmpdir/demux $OUTDIR
+
